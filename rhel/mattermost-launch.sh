b7b5ee64a4d1582db24538e53f357568ff1c4227
Grigoriy I <g.duke253@gmail.com>
2021-01-12 20:38:03 +0300

    comments added

--- rhel/mattermost-launch.sh
+++ rhel/mattermost-launch.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+echo -ne "Starting launch script..."
 MM_CONFIG=${MM_CONFIG:-/opt/mattermost/config.json}
+echo "done"
 
+echo -ne "Switching to user mattermost..."
 # Switch to user mattermost
 exec sudo -iu mattermost
+echo "done"
 
+echo -ne "Starting mattermost server..."
 # Start mattermost server
 exec /opt/mattermost/bin/platform -c $MM_CONFIG
+echo "done"
