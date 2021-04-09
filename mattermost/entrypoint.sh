151f83d012dd4ddb7b1f6b832e2a1adcd0fd8c99
Grigoriy I <g.duke253@gmail.com>
2021-04-09 18:55:50 +0300

    ...

--- mattermost/entrypoint.sh
+++ mattermost/entrypoint.sh
@@ -84,7 +84,9 @@ if [ ! -f $MM_CONFIG ]; then
 	jq '.TeamSettings.TeammateNameDisplay = "full_name"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
 	jq '.PluginSettings.EnableUploads = "false"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
 	jq '.PluginSettings.PluginStates += {"mattermost-teamcity-plugin": {"Enable": "true"}}' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
-	jq '.PluginSettings.Plugins += {"mattermost-teamcity-plugin": {"teamcitymaxbuilds": "5", "teamcitytoken": "'$TEAMCITY_AUTH_TOKEN'", "teamcityurl": "'$TEAMCITY_URL'"}}' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+	jq '.PluginSettings.Plugins += {"mattermost-teamcity-plugin": {"teamcitymaxbuilds": "5", "teamcitytoken": "", "teamcityurl": ""}}' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+	jq '.PluginSettings.Plugins.mattermost-teamcity-plugin.teamcitytoken = "'$TEAMCITY_AUTH_TOKEN'"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+	jq '.PluginSettings.Plugins.mattermost-teamcity-plugin.teamcityurl = "'$TEAMCITY_URL'"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
 
 	echo "done"
 else
